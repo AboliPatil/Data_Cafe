f4509800bc1ce0828a3324a6c75841c16b0d9fd3
sjamadari15 <siddharth.jamadari@clairvoyantsoft.com>
2023-01-25 15:23:51 +0530

    added missing parameter

--- retail_data_platform/src/main.py
+++ retail_data_platform/src/main.py
@@ -7,7 +7,7 @@ from src.com.cv.utils import spark_utils
 from src.com.cv.utils import utils
 
 
-def main(default, table_config):
+def main(default, table_config, table_name):
     spark = spark_utils.get_spark_session(default, table_name)
     df = gcs_utils.read_gcs(spark, table_config, default)
     bigquery_utils.write_bigquery(spark, df, table_config, default)
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     tables_run_seq = default["tables_run_seq"].split(",")
     for table_seq in tables_run_seq:
         table_config = config[table_seq]
-        main(default, table_config)
+        main(default, table_config, table_seq)
